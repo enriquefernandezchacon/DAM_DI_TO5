b688c4f38821dc9f5354fc600e777c4af1111edc
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-03-01 08:49:45 +0100

    Terminada BD

---
+++ DI5/Citas.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].Citas(
+	[id] INT NOT NULL, 
+    [fecha] DATE NOT NULL,
+    [hora] TIME NOT NULL,
+    [forma_pago] VARCHAR(50) NOT NULL,
+    [id_cliente] INT NOT NULL,
+    [id_tratamiento] INT NOT NULL,
+    [observaciones] VARCHAR(500) NULL,
+    CONSTRAINT [PK_Citas] PRIMARY KEY ([id]),
+    CONSTRAINT [FK_Citas_Clientes] FOREIGN KEY ([id_cliente]) REFERENCES [dbo].Clientes([id]),
+    CONSTRAINT [FK_Citas_Tratamientos] FOREIGN KEY ([id_tratamiento]) REFERENCES [dbo].Tratamientos([id])
+);
---
+++ DI5/Empleados.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].Empleados(
+	[id] INT NOT NULL, 
+    [nombre] VARCHAR(50) NOT NULL,
+	[apellidos] VARCHAR(100) NOT NULL,
+	[dni] VARCHAR(9) NOT NULL,
+	[email] VARCHAR(100) NOT NULL,
+	[telefono] VARCHAR(20) NOT NULL,
+	[puesto] VARCHAR(50) NOT NULL,
+	CONSTRAINT [PK_Empleados] PRIMARY KEY ([id])
+);
